932ba580ed8122f9b31a361c3c5538fcf59f6759
therazerhub <therazerhub@gmail.com>
2026-09-13 16:45:08 +0530

    Pin cinemagoer==2023.5.1 (last version with HTTP scraper)

--- utils.py
+++ utils.py
@@ -20,7 +20,7 @@ logger.setLevel(logging.INFO)
 join_db = JoinReqs
 BTN_URL_REGEX = re.compile(r"(\[([^\[]+?)\]\((buttonurl|buttonalert):(?:/{0,2})(.+?)(:same)?\))")
 
-imdb = Cinemagoer()
+imdb = Cinemagoer('http')
 TOKENS = {}
 VERIFIED = {}
 BANNED = {}
